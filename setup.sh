29dc6f6863f3740080b54b92c2fc3222295928ee
Takafumi Umemoto <takafumi.umemotoh@gmail.com>
2020-09-20 11:35:14 +0900

    ディレクトリがなかった場合に作成する処理を追加

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 cd $(dirname $0)
 for dotfile in $(find . -type f | egrep -v './.git/|setup.sh|README.md' | sed 's/.\///')
 do
+    if [ ! -e $(dirname $HOME/$dotfile) ]; then
+        mkdir -p $(dirname $HOME/$dotfile)
+    fi
     ln -s "$PWD/$dotfile" $HOME/$dotfile
 done
-cd - 
+cd -
