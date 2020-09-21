3bf5ac5920f38e9d91c4256ecd545b95219b7a90
Takafumi Umemoto <takafumi.umemotoh@gmail.com>
2020-09-21 13:46:38 +0900

    VSCodeのUser設定を追加

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
  #!/bin/bash
 
 cd $(dirname $0)
-for dotfile in $(find . -type f | egrep -v './.git/|setup.sh|README.md' | sed 's/.\///')
+for dotfile in $(find . -type f | egrep -v './.git/|setup.sh|README.md' | sed 's/.\///'  | grep -E '^\..*$')
 do
     if [ ! -e $(dirname $HOME/$dotfile) ]; then
         mkdir -p $(dirname $HOME/$dotfile)
     fi
     ln -s "$PWD/$dotfile" $HOME/$dotfile
 done
+
+# VSCode User 設定(User and WorkSpace Settings https://code.visualstudio.com/docs/getstarted/settings)
+ln -s $PWD/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 cd -
